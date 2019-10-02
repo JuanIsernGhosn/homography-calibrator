bff81e3d96612e387df94e7e3097f17974344a06
jisern <jisern@correo.ugr.es>
2019-10-02 13:11:16 +0200

    Added JSON file read function

--- src/Model/JSON/jsonserializer.py
+++ src/Model/JSON/jsonserializer.py
@@ -4,7 +4,7 @@ def serialize_data(homography):
     json_ser={
         "image_name": homography.image.get().filename,
         "px": homography.px.get().tolist(),
-        "coord": homography.coord.tolist(),
+        "coord": homography.coord.get().tolist(),
         "h_matrix": homography.h.get().tolist()
     }
     return json_ser
@@ -12,3 +12,8 @@ def serialize_data(homography):
 def save_json_file(json_data, filename):
     with open(filename, 'w') as json_file:
         json.dump(json_data, json_file)
+
+def read_json_file(filename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+    return data
