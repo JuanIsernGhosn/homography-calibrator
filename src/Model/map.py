223e5ca14deb9514fef247fe0f20237898b5031a
jisern <jisern@correo.ugr.es>
2020-01-16 14:03:27 +0100

    Added map class for wrapping properties

---
+++ src/Model/map.py
@@ -0,0 +1,10 @@
+
+class Map:
+
+    def __init__(self, lat=None, lon=None, zoom=None, map_type=None, img=None):
+        self.img = img
+        self.lat = lat
+        self.lon = lon
+        self.zoom = zoom
+        self.map_type = map_type
+        self.img = img
