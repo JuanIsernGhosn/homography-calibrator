1626e403c7f5958419e741f1f4d2fc2aec84fbb1
jisern <jisern@correo.ugr.es>
2020-01-20 14:01:15 +0100

    Minor changes

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ def main():
     root = Tk()
     app = Controller(root)
     app.change_camera_image('/home/jisern/repositories/homography-calibrator/media/title_image.jpg')
-    app.change_bird_view_image(lat=37.088012, lon=-3.751100, zoom=20, maptype="satellite")
+    app.change_bird_view_image(lat=41.85, lon=-87.65, zoom=14, maptype="satellite")
     app.view.root.mainloop()
     return 0
 
