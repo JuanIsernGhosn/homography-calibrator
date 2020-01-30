9e86bda379745fe89936c46718d03ade01cf7780
jisern <jisern@correo.ugr.es>
2020-01-30 13:46:54 +0100

    Added functions for serializing and deserializing configuration files

--- src/Model/JSON/jsonserializer.py
+++ src/Model/JSON/jsonserializer.py
@@ -1,17 +1,33 @@
 import json
 
-def serialize_homo_data(homography):
+
+def serialize_homo_data(homography, bird_calculator):
     json_ser={
         "image_name": homography.image.get().filename,
-        "px": homography.px.get().tolist(),
+        "map_coord": (bird_calculator.map.get().lat, bird_calculator.map.get().lon),
+        "map_zoom": bird_calculator.map.get().zoom,
+        "map_marks_px": homography.px_bird.get().tolist(),
+        "camera_marks_px": homography.px.get().tolist(),
         "coord": homography.coord.get().tolist(),
         "h_matrix": homography.h.get().tolist()
     }
     return json_ser
 
+
+def serialize_per_data(per_manager, bird_calculator):
+    json_ser={
+        "map_coord": (bird_calculator.map.get().lat, bird_calculator.map.get().lon),
+        "map_zoom": bird_calculator.map.get().zoom,
+        "perimeters": str(per_manager.perimeters.get()),
+        "perimeters_px": str(per_manager.perimeters_px.get())
+    }
+    return json_ser
+
+
 def save_json_file(json_data, filename):
     with open(filename, 'w') as json_file:
         json.dump(json_data, json_file)
+
 
 def read_json_file(filename):
     with open(filename) as json_file:
--- src/Model/JSON/modeldeserializer.py
+++ src/Model/JSON/modeldeserializer.py
@@ -1,15 +1,39 @@
 from src.Model.homographycalculator import HomographyCalculator
+from src.Model.perimetermanager import PerimeterManager
+from src.Model.birdcalculator import BirdCalculator
 import numpy as np
+import ast
 
-class CalculatorDeserializer:
+
+class HomographyDeserializer:
+
     def deserialize(self, json):
-        calculator = HomographyCalculator()
-        calculator.px.set(np.array(json['px'], dtype=np.float32))
-        calculator.h.set(np.array(json['h_matrix'], dtype=np.float32))
-        calculator.coord.set(np.array(json['coord'], dtype=np.float32))
+        homo_calculator = HomographyCalculator()
+        homo_calculator.px.set(np.array(json['camera_marks_px'], dtype=np.float32))
+        homo_calculator.h.set(np.array(json['h_matrix'], dtype=np.float32))
+        homo_calculator.coord.set(np.array(json['coord'], dtype=np.float32))
+        homo_calculator.px_bird.set(np.array(json['map_marks_px'], dtype=np.float32))
 
-        return calculator
+        bird_calculator = BirdCalculator()
+        (lat, lon) = json['map_coord']
+        zoom = json['map_zoom']
+        bird_calculator.change_map(lat, lon, zoom)
+
+        return homo_calculator, bird_calculator
 
 
+class PerimetersDeserializer:
 
+    def deserialize(self, json):
+        per_manager = PerimeterManager()
+        perimeters_px = ast.literal_eval(json['perimeters_px'])
+        perimeters = ast.literal_eval(json['perimeters'])
+        per_manager.perimeters.set(perimeters)
+        per_manager.perimeters_px.set(perimeters_px)
 
+        bird_calculator = BirdCalculator()
+        (lat, lon) = json['map_coord']
+        zoom = json['map_zoom']
+        bird_calculator.change_map(lat, lon, zoom)
+
+        return per_manager, bird_calculator
