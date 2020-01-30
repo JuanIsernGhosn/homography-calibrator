8f0f9636bc4a06188da082140f655f484cbcc4c2
jisern <jisern@correo.ugr.es>
2020-01-30 13:43:41 +0100

    Changed initial coordinates

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ def main():
     root = Tk()
     app = Controller(root)
     app.change_camera_image('../media/title_image.jpg')
-    app.change_homo_bird_view_image(lat=41.85, lon=-87.65, zoom=20, maptype="satellite")
-    app.change_peri_bird_view_image(lat=41.85, lon=-87.65, zoom=20, maptype="satellite")
+    app.change_homo_bird_view_image(lat=0, lon=0, zoom=1, maptype="satellite")
+    app.change_peri_bird_view_image(lat=0, lon=0, zoom=1, maptype="satellite")
     app.view.root.mainloop()
     return 0
 
